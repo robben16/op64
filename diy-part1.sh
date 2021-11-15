f4a0e1fbd51747cde62751642ae41132e82b42db
robben16 <robben16@users.noreply.github.com>
2021-11-15 22:13:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git lienol https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
